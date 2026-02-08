d478ee803f5693f793cc8864786bdf99d05d6ef4
Maral katebi <marall@live.com>
2026-02-08 11:16:35 +0330

    added customer domain

---
+++ CQRS/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Customers.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customers.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CQRS/Data/Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Customers.Data.Model
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Firstname { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Lastname { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Email { get; set; }
+    }
+}
